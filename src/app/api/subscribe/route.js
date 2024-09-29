cf1d44bfecf78e4866a2a25991a3e818a55b8778
Jay <mohamya77@gmail.com>
2024-09-29 19:15:48 +0300

    reordering operations

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -1,37 +1,6 @@
 import { NextResponse } from 'next/server';
 import startMongo from '../../../../utilis/startMongo';
 
-export async function GET(req) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const name = searchParams.get('name');    
-    const surname = searchParams.get('surname');
-
-    if (!name || !surname) {
-      return NextResponse.json({ error: 'Name and surname are required' }, { status: 400, headers: { 'Cache-Control': 'no-store, max-age=0' } });
-    }
-
-    const client = await startMongo();
-    const db = client.db('ieee');
-
-    const existingSubscriber = await db.collection('subscribers').findOne({
-      name: name,
-      surname: surname,
-    });
-
-    client.close();
-
-    if (existingSubscriber) {
-      return NextResponse.json({ exists: true }, { status: 200, headers: { 'Cache-Control': 'no-store, max-age=0' } });
-    } else {
-      return NextResponse.json({ exists: false }, { status: 200, headers: { 'Cache-Control': 'no-store, max-age=0' } });
-    }
-  } catch (e) { 
-    console.error(e);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500, headers: { 'Cache-Control': 'no-store, max-age=0' } });
-  }
-}
-
 export async function POST(req) {
   try {
     const { email, name, surname } = await req.json();
@@ -53,10 +22,6 @@ export async function POST(req) {
       return NextResponse.json({ message: 'Email is already subscribed.' }, { status: 409, headers: { 'Cache-Control': 'no-store, max-age=0' } });
     }
 
-    // Add new subscriber to MongoDB
-    await db.collection('subscribers').insertOne({ email, name, surname });
-    client.close();
-
     // Send the subscriber data to Brevo (Sendinblue)
     const brevoResponse = await fetch('https://api.brevo.com/v3/contacts', {
       method: 'POST',
@@ -74,10 +39,16 @@ export async function POST(req) {
     if (!brevoResponse.ok) {
       const errorData = await brevoResponse.json();
       console.error('Error from Brevo:', errorData);
+      client.close(); // Close Mongo client if Brevo request fails
       return NextResponse.json({ message: 'Failed to add contact to Brevo.', error: errorData }, { status: 500, headers: { 'Cache-Control': 'no-store, max-age=0' } });
     }
 
+    // If Brevo request is successful, add new subscriber to MongoDB
+    await db.collection('subscribers').insertOne({ email, name, surname });
+    client.close();
+
     return NextResponse.json({ message: 'Subscription successful!' }, { status: 200, headers: { 'Cache-Control': 'no-store, max-age=0' } });
+
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: 'Internal Server Error', details: e.message }, { status: 500, headers: { 'Cache-Control': 'no-store, max-age=0' } });
